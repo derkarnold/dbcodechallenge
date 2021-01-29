28d8bd87372f01a0f56e4f6cf9f64ebf0818e187
Deryck Arnold <derkarnold@gmail.com>
2021-01-29 11:03:34 +0000

    Fix mess.

--- puzzles/puzzle3.py
+++ puzzles/puzzle3.py
@@ -1,5 +1,6 @@
 # March 13th 1**4, on a leap year is "The third youngest".
 # Hint is "the youngest"...
+# https://challenge.dreambroker.jobs/af776836-ae78-45b6-be76-079b557460ed
 import datetime as dt
 
 _IDX_OF_TUESDAY = 1
@@ -12,4 +13,5 @@ for year in range(1004, 2004, 10):
     birthDate = dt.date(year, 3, 13)
     if birthDate.weekday() != _IDX_OF_TUESDAY:
         continue
-    print(year, birthDate + dt.timedelta(days=366 * 
+    print(year)
+    
